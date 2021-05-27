fa8be9d8c684acc7a665afbc04edc07f7d1f587f
budlebee <bbogle7613@gmail.com>
2021-05-27 22:08:18 +0900

    todo: using honeysql

--- project.clj
+++ project.clj
@@ -6,11 +6,13 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.4.0"]
+                 [ring-cors "0.1.13"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
-  :plugins [[lein-ring "0.12.5"]
-            [lein-beanstalk "0.2.7"]]
-  :ring {:handler cheshire-cat.handler/app}
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [com.github.seancorfield/honeysql "2.0.0-rc2"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler cheshire-cat.handler/app
+         :port 8000}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -4,7 +4,11 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]
             [ring.middleware.json :as ring-json]
             [ring.util.response :as rr]
+            [ring.middleware.cors :refer [wrap-cors]]
+
             [clojure.java.jdbc :as sql]))
+
+(def db "postgresql://localhost:5432/testdb")
 
 (defroutes app-routes
   (GET "/" [] (do (println "someone comes in!")
@@ -18,8 +22,37 @@
                             ["select * from testing"]))
         (str "wow" "<h1>hello!</h1>" "data in DB: " (:data (first (sql/query "postgresql://localhost:5432/testdb"
                                                                              ["select * from testing"]))))))
-  (POST "/post" []
-    (rr/response {:name "post" :status "good"}))
+  (POST "/get-sentence" req
+    (println (type (sql/query db
+                              ["select * from sentences"])))
+    (rr/response (sql/query db
+                            ["select * from sentences"])))
+  (POST "/add-sentence" req
+    ;(def addedBy (get (get req :body) "addedBy"))
+    ; addedby 에는 사용자 id 값, int 가 들어가야함.
+    (def text (get (get req :body) "text"))
+    (def con (sql/get-connection db))
+    (def val-to-insert
+      (.createArrayOf con "int" (into-array Integer [])))
+    (sql/insert! db
+                 :sentences {:text text :addedby 1234 :lovers val-to-insert :haters val-to-insert})
+    (rr/response {:addedBy 1234 :text text}))
+  (PUT "/love-sentence" req
+    ;(def addedBy (get (get req :body) "addedBy"))
+    ;(def bodyText (get (get req :body) "bodyText"))
+    ;(rr/response {:addedBy addedBy :bodyText bodyText})
+    )
+  (PUT "/hate-sentence" req
+    ;(def addedBy (get (get req :body) "addedBy"))
+    ;(def bodyText (get (get req :body) "bodyText"))
+    ;(rr/response {:addedBy addedBy :bodyText bodyText})
+    )
+  (POST "/signup" req
+    (def nickname (get (get req :body) "nickname"))
+    (def email (get (get req :body) "email"))
+    (sql/insert! db
+                 :users {:email email :nickname nickname})
+    (rr/response {:email email :nickname nickname}))
   (PUT "/put" []
     (rr/response {:name "put" :status "good"}))
   (DELETE "/delete" []
@@ -40,5 +73,7 @@
 
 (def app
   (-> app-routes
+      (ring-json/wrap-json-body)
       (ring-json/wrap-json-response)
-      (wrap-defaults api-defaults)))
+      (wrap-defaults api-defaults)
+      (wrap-cors :access-control-allow-origin [#".*"] :access-control-allow-methods [:get :post])))

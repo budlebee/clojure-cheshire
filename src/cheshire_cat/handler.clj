426c65b303290416c51a26d113f57733ffcb9d71
budlebee <bbogle7613@gmail.com>
2021-07-05 15:01:52 +0900

    done: get-sentence 쿼리에서 단순 인기도에 따른 랭킹 부여

--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -23,8 +23,12 @@
       (str "wow" "<h1>hello!</h1>" "data in DB: " (:data (first (sql/query "postgresql://localhost:5432/testdb"
                                                                            ["select * from sentences"]))))))
   (POST "/get-sentence" req
+    (def user-id
+      (Integer/parseInt (get-in req [:body "userId"])))
+    ;(def result
+     ; (sql/query db [(format "select * from posts where id not in ((select post_id from loves where user_id=%s) union (select post_id from hates where user_id=%s))" user-id user-id)]))
     (def result
-      (sql/query db ["select id, content, created_by from posts"]))
+      (sql/query db [(format "select * from posts left outer join (select post_id, count (*) from loves group by post_id) as ranking on posts.id=ranking.post_id where id not in ((select post_id from loves where user_id=%s) union (select post_id from hates where user_id=%s)) order by ranking.count asc" user-id user-id)]))
     (rr/response {:result result}))
 
   (POST "/add-sentence" req
@@ -57,7 +61,7 @@
     (def user-id
       (Integer/parseInt (get-in req [:body "userId"])))
     (def result
-      (sql/query db [(format "select * from loves left outer join posts on loves.post_id = posts.id where loves.user_id=%s" user-id)]))
+      (sql/query db [(format "select * from loves left outer join posts on loves.post_id = posts.id where loves.user_id=%s and loves.deleted=false order by loves.id desc" user-id)]))
     (rr/response {:result result}))
 
   (POST "/signup" req

a5ecf0a69d5b9afbdcb43fbd58853ae8429f2b36
budlebee <bbogle7613@gmail.com>
2021-07-05 15:36:09 +0900

    done : 팔로우 기능과 팔로우한 사람의 글만 가져오기 추가.

--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -34,12 +34,12 @@
   (POST "/add-sentence" req
     (def content
       (get-in req [:body "content"]))
-    (def userId
+    (def user-id
       (Integer/parseInt (get-in req [:body "userId"])))
-    (def post_id (get-in (first (sql/insert! db
-                                             :posts {:content content :created_by userId} {:return-keys ["id"]})) [:id]))
-    (sql/insert! db :loves {:user_id userId :post_id post_id})
-    (rr/response {:content content :userId userId}))
+    (def post-id (get-in (first (sql/insert! db
+                                             :posts {:content content :created_by user-id} {:return-keys ["id"]})) [:id]))
+    (sql/insert! db :loves {:user_id user-id :post_id post-id})
+    (rr/response {:content content :userId user-id}))
 
   (POST "/love-sentence" req
     (def post-id
@@ -63,6 +63,27 @@
     (def result
       (sql/query db [(format "select * from loves left outer join posts on loves.post_id = posts.id where loves.user_id=%s and loves.deleted=false order by loves.id desc" user-id)]))
     (rr/response {:result result}))
+
+  (POST "/follow" req
+    (def guru-id
+      (Integer/parseInt (get-in req [:body "guruId"])))
+    (def follower-id
+      (Integer/parseInt (get-in req [:body "userId"])))
+    (sql/insert! db :followers {:guru_id guru-id :follower_id follower-id})
+    (rr/response {:guru_id guru-id :follower_id follower-id}))
+
+  (POST "/gurus-feed" req
+    (def user-id
+      (Integer/parseInt (get-in req [:body "userId"])))
+
+   ; (format "select * from posts where created_by in (select guru_id from followers where follower_id=%s) order by timestamp desc" user-id)
+    (def result
+      (sql/query db [(format "select * from posts where created_by in (select guru_id from followers where follower_id=%s) order by timestamp desc" user-id)]))
+    ; posts 에서 guru 가 쓴것들만 timestamp 순으로 정렬하면 됨.
+
+    (rr/response {:result result}))
+
+
 
   (POST "/signup" req
     (def nickname (get (get req :body) "nickname"))
